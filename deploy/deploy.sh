d8d071eaf58c8c436f9ae8be4cea020108826b70
barbz <eduardob907@gmail.com>
2025-04-02 23:54:06 +0200

    chore: remove env variable for deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,7 +12,6 @@ docker rm web-sports-elo &>/dev/null || true
 
 docker run -d --restart=always -p 3000:3000 --name web-sports-elo \
   --network sports-elo-network \
-  -e API_URL=${API_URL} \
   baaarbz/web-sports-elo:latest
 
 docker images --filter "dangling=true" -q | xargs -r docker rmi
